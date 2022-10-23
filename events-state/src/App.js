b8eef88ffcf737c66e9f4b5ef4fe98228c532736
Oleg Zasedatelew <ol.zasedatelew@yandex.ru>
2022-10-23 18:42:58 +0300

    1 task second lesson added

--- events-state/src/App.js
+++ events-state/src/App.js
@@ -1,12 +1,22 @@
 import './App.scss';
+import React from 'react';
+import axios from 'axios';
 import Portfolio from './components/Portfolio';
 
 function App() {
+  const [projects, setProjects] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/items.json').then(({ data }) => {
+      setProjects(data.items);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <Portfolio 
-        filters={["All", "Websites", "Flayers", "Business Cards"]}/>
+        filters={["All", "Websites", "Flayers", "Business Cards"]}
+        projects={projects} />
       </header>
     </div>
   );
